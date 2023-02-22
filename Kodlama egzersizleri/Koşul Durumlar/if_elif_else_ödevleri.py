9885c8b6b7ffeb87a651d6b1176b2748ee17e614
Tuğba ANBAR <tugbademrell@gmail.com>
2023-02-22 16:34:46 +0200

    if_elif_else_ödevleri_2_3

--- Kodlama egzersizleri/Koşul Durumlar/if_elif_else_ödevleri.py
+++ Kodlama egzersizleri/Koşul Durumlar/if_elif_else_ödevleri.py
@@ -8,11 +8,11 @@ Kullanıcıdan alınan boy ve kilo değerlerine göre beden kitle indeksini hesa
  BKİ 30'un üstündeyse -------------> Obez
 """
 
-"""
-print("""*********************
-Beden Kitle Endeksi Hesaplama
-*********************""")
 
+#print("""*********************
+#Beden Kitle Endeksi Hesaplama
+#*********************""")
+"""
 boy = float(input("Boyunuz:"))
 kilo = int(input("Kilonuz:"))
 
@@ -27,5 +27,53 @@ elif (beden_kitle_endeksi >= 25 and beden_kitle_endeksi < 30):
     print("Fazla kilolu")
 else:
     print("Obez")
+
+#Problem 2 -- Kullanıcıdan 3 tane sayı alın ve en büyük sayıyı ekrana yazdırın.
+"""
+"""
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
+
+if (a >= b and a >= c):
+    print("En büyük sayı:",a)
+elif (b >= a and b>= c):
+    print("En büyük sayı:",b)
+elif (c >= a and c >= b):
+    print("En büyük say:",c)
 """
 
+"""
+Problem 3
+Kullanıcının girdiği vize1,vize2,final notlarına notlarına göre harf notunu hesaplayın.
+Vize1 toplam notun %30'una etki edecek.
+Vize2 toplam notun %30'una etki edecek.
+Final toplam notun %40'ına etki edecek
+"""
+
+vize1 = int(input("1.Vize:"))
+vize2 = int(input("2.Vize:"))
+final = int(input("Final Notu:"))
+
+toplam_not = (vize1 * 30 /100) + (vize2 * 30 / 100) + (final * 40 /100)
+
+print("Toplam not:",toplam_not)
+
+if(toplam_not >= 90):
+    print("Harf notunuz: AA")
+elif(toplam_not >= 85):
+    print("Harf notunuz: BA")
+elif(toplam_not >= 80):
+    print("Harf notunuz: BB")
+elif(toplam_not >= 75):
+    print("Harf notunuz: CB")
+elif(toplam_not >= 70):
+    print("Harf notunuz: CC")
+elif(toplam_not >= 65):
+    print("Harf notunuz: DC")
+elif(toplam_not >= 60):
+    print("Harf notunuz: DD")
+elif(toplam_not >= 55):
+    print("Harf notunuz: FD")
+else:
+    print("Harf notunuz: FF")
